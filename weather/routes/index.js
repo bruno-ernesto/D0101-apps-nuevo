6f6f31756f70bd00e7db356e0f2b1a4e6218b8ff
bruno-ernesto <87513552+bruno-ernesto@users.noreply.github.com>
2022-06-29 17:03:34 -0500

    Update index.js

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetch = require("node-fetch");
 require('dotenv').config();
-const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key';
+const OWM_API_KEY = process.env.OWM_API_KEY || 'invalid_key'|| 'd2a4494385d1cbb41160ebd457d9a10e';
 const UNITS = process.env.UNITS || 'metric';
 
 /* GET home page. */
