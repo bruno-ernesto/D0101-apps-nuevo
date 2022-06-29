94aa1292d2135cd65e8eff5442118d6ae83ae968
bruno-ernesto <87513552+bruno-ernesto@users.noreply.github.com>
2022-06-29 14:39:48 -0500

    funcion 2

--- version/app.js
+++ version/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 app = express();
 
 var response;
-
+/*
 app.get('/', function (req, res) {
 
     response = 'This is version 1 of the app.' + '\n';
@@ -11,6 +11,16 @@ app.get('/', function (req, res) {
     res.send(response);
 
 });
+*/
+app.get('/', function (req, res) {
+
+    response = 'This is version 2 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
+
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
